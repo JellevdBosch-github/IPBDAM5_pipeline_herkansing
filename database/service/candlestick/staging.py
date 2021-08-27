38bef9e957aaea74cd35cf456ab2c2532e4f038f
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-08-27 09:52:34 +0200

    ETL stream service | Added edit() and a read() function

--- database/service/candlestick/staging.py
+++ database/service/candlestick/staging.py
@@ -18,3 +18,34 @@ def add(dataset):
 			f"open_timestamp = '{dataset['open_timestamp']}', "
 			f"close_timestamp = '{dataset['close_timestamp']}' "
 		)
+
+
+def edit(c_id, pattern, signal):
+	with Connection('staging').get_connection() as conn:
+		conn.execute(
+			f"UPDATE candlestick SET "
+			f"pattern = '{pattern}', "
+			f"signal = '{signal}' "
+			f"WHERE candlestick_id = {c_id};"
+		)
+
+
+def read_last_3():
+	with Connection('staging').get_connection() as conn:
+		result = conn.execute(
+			f"SELECT * FROM ("
+			f"SELECT * FROM candlestick ORDER BY open_timestamp DESC LIMIT 3"
+			f") AS r ORDER BY open_timestamp"
+		)
+		candles = []
+		for r in result:
+			candles.append({
+				'candlestick_id': r[0],
+				'open': r[1],
+				'high': r[2],
+				'low': r[3],
+				'close': r[4],
+				'open_timestamp': r[5],
+				'close_timestamp': r[6],
+			})
+		return candles
