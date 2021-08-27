c4ba8c49c230e4a4ce0fae9d352957a75c509e6b
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-08-27 09:53:01 +0200

    ETL Wallet service | Added Wallet Service

---
+++ database/service/wallet/staging.py
@@ -0,0 +1,28 @@
+from . import Connection
+
+
+def edit(wallet, action):
+	with Connection('staging').get_connection() as conn:
+		if action == 'buy':
+			conn.execute(
+				f"UPDATE wallet SET "
+				f"eur_value = eur_value - {wallet['eur_value']}, "
+				f"doge_value = doge_value - {wallet['doge_value']}"
+			)
+		else:
+			conn.execute(
+				f"UPDATE wallet SET "
+				f"eur_value = eur_value + {wallet['eur_value']}, "
+				f"doge_value = doge_value + {wallet['doge_value']}"
+			)
+
+
+def read(wallet_id):
+	with Connection('staging').get_connection() as conn:
+		result = conn.execute(
+			f"SELECT eur_value FROM wallet WHERE wallet_id = '{wallet_id}'"
+		)
+		eur_value = 0.00
+		for r in result:
+			eur_value = r
+		return eur_value
