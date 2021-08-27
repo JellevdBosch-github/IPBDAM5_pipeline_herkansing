d677c13a683cbea5edf32e030a89399659883288
JellevdBosch-github <jellevandenbosch@outlook.com>
2021-08-27 09:53:20 +0200

    Create Trade | Created first version for etl stream data

--- pipeline_components/staging_master/task/trade/create_trade.py
+++ pipeline_components/staging_master/task/trade/create_trade.py
@@ -1,43 +1,63 @@
 import pandas as pd
 from uuid import uuid4
 import utils
+from database.service.trade import staging as trade_service
+from database.service.wallet import staging as wallet_service
 
 
 class CreateTrade:
 
-	def __init__(self, pattern, signal, original):
+	def __init__(self, pattern, signal, original_id, candlestick_id):
 		self.pattern = pattern
 		self.signal = signal
-		self.original = original
+		self.original_id = original_id
+		self.candlestick_id = candlestick_id
 		self.id = uuid4()
+		self.wallet_id = '99922c40-700b-49f3-ad16-2dd201750ddb'
+		self.currency_id = 'DOGEEUR'
+		self.timestamp = utils.get_current_epoch_ms()
+
+	def get_id(self):
+		return self.id
 
 	def trade(self):
-		trade = {}
 		# If it's a secondary trade
-		if self.original:
-			#		trade['eur_value'] = utils.convert_currency('DOGE', 'EUR', self.doge_value)  # TODO get the dogeamount from somewhere
-			# 		trade['doge_value'] = self.doge_value  # TODO get the dogeamount from somewhere
-			pass
+		if self.original_id:
+			doge_value = trade_service.read(self.original_id)
+			eur_value = utils.convert_currency('DOGE', 'EUR', doge_value)
 		# If it's a primary trade
 		else:
-			trade['eur_value'] = 100.00
-			trade['doge_value'] = round(utils.convert_currency('EUR', 'DOGE', trade['eur_value']), 10)
-		# TODO make the trade
+			eur_value = 100.00
+			doge_value = round(utils.convert_currency('EUR', 'DOGE', eur_value), 10)
+
+		eur_value = round(eur_value, 2)
+		trade = {
+			'trade_id': self.id,
+			'wallet_id': self.wallet_id,
+			'currency_id': self.currency_id,
+			'candlestick_id': self.candlestick_id,
+			'pattern': self.pattern,
+			'signal': self.signal,
+			'eur_value': eur_value,
+			'doge_value': doge_value,
+			'timestamp': self.timestamp
+		}
+		wallet = {
+			'eur_value': eur_value,
+			'doge_value': doge_value,
+		}
+
+		wallet_service.edit(wallet, 'buy') if self.signal == 'bull' else wallet_service.edit(wallet, 'sell')
+
 		# If this is a secondary trade
-		if self.original:
-			# TODO create trade
-			# If the original signal was bullish, then now you have to buy back
+		if self.original_id:
+			trade['original_id'] = self.original_id
+			trade['wallet_value_eur'] = wallet_service.read(self.wallet_id)
+			# Calculate the profits
 			if self.signal == 'bull':
-				# TODO update wallet
-				pass
+				trade['profit'] = trade['eur_value'] - 100.00
 			else:
-				# TODO update wallet
-				pass
+				trade['profit'] = 100.00 - trade['eur_value']
+			trade_service.add_secondary(trade)
 		else:
-			# TODO create trade
-			if self.signal == 'bull':
-				# TODO update wallet
-				pass
-			else:
-				# TODO update wallet
-				pass
+			trade_service.add(trade)
